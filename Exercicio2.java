e24a3e76403bb2f438fc0aff82f139fd77f3ce0b
Evelyn <128431672+evyamoraes@users.noreply.github.com>
2023-04-26 18:32:48 -0300

    Update Exercicio2.java

--- Exercicio2.java
+++ Exercicio2.java
@@ -7,7 +7,7 @@ public class Exercicio2 {
 	public static void main(String[] args) {
 		// Faça um sistema que leia a idade de uma pessoa expressa em dias e mostre-a
 		// expressa em anos, meses e dias.
-		;
+		
 		Scanner input = new Scanner(System.in);{
 
 		int idade;
