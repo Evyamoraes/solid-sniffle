f13bf53222e7d0b5591f48e15894b5fda1026dfc
Evelyn <128431672+evyamoraes@users.noreply.github.com>
2023-04-26 22:18:50 -0300

    Add files via upload

---
+++ soma_multiplos.java
@@ -0,0 +1,27 @@
+package soma_multiplos;
+
+import java.util.Scanner;
+
+public class soma_multiplos {
+
+	public static void main(String[] args) {
+		// Desenvolver um sistema que efetue a soma de todos os números ímpares que são  múltiplos de três
+		// e que se encontram no conjunto dos números de 1 até 500.
+		
+		Scanner input = new Scanner(System.in);{
+			
+			int soma = 0;
+		
+				for(int i = 0; i <= 500; i+=3) {
+					if (i % 2 != 0);{
+					soma = soma + i;
+					System.out.println(i);
+					}
+			}
+
+		System.out.println("A soma de todos o números ímpares múltiplos de três entre 1 até 500 é: " +soma);
+		input.close();
+	
+	}
+	}
+}
