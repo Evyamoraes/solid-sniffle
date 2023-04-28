2d758a1cc7d2ae7c63df34a035718a0ed1245976
Evelyn <128431672+evyamoraes@users.noreply.github.com>
2023-04-27 21:03:28 -0300

    Add files via upload

---
+++ exercicio5while.java
@@ -0,0 +1,37 @@
+package exercicio5while;
+
+import java.util.Scanner;
+
+public class exercicio5while {
+
+	public static void main(String[] args) {
+		// Faça um programa que mostre uma contagem na tela de 233 a 456, só que contando de 3 em 3
+		// quando estiver entre 300 e 400 e de 5 em 5 quando não estiver.
+		
+		Scanner input = new Scanner(System.in);{
+			
+		int numero = 233;
+				
+		System.out.println(+numero);
+		System.out.println("\n");
+	
+				do{
+
+					if (numero >= 300 && numero <= 400) {
+						numero += 3;
+						System.out.println(numero);
+						System.out.println(" somando de 3 em 3\n");
+					}
+					else{
+						numero +=5;
+						System.out.println(numero);
+						System.out.println(" somando de 5 em 5\n");
+					}
+					
+				}
+					while (numero <= 456);
+			}
+	}
+
+}
+
